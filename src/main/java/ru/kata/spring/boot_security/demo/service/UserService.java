c2ae34d1e9cc0edd007e34fabd1e5369c47732c5
Oleganya123 <moonka321@mail.ru>
2025-03-26 14:01:21 +0300

    bootstrap test

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -1,13 +1,13 @@
-    package ru.kata.spring.boot_security.demo.service;
+package ru.kata.spring.boot_security.demo.service;
 
-    import ru.kata.spring.boot_security.demo.model.User;
-    import java.util.List;
+import ru.kata.spring.boot_security.demo.model.User;
+import java.util.List;
 
-    public interface UserService {
-        List<User> getAllUsers();
-        User getUserById(Long id);
-        User findByEmail(String name);
-        void addUser(User user);
-        void updateUser(User user, String newPassword);
-        void deleteUser(Long id);
-    }
+public interface UserService {
+    List<User> getAllUsers();
+    User getUserById(Long id);
+    User findByEmail(String name);
+    void addUser(User user);
+    void updateUser(User user, String newPassword);
+    void deleteUser(Long id);
+}
